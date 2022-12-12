AsanTU/TM5

Homework 7 / Registration

app/src/main/kotlin/com/chat/homeworks/data/models/Users.kt
@@ -1,0 +1,6 @@
+package com.chat.homeworks.data.models
+
+data class Users(
+    var phone: String? = null,
+    var uid: String? = null
+)

app/src/main/kotlin/com/chat/homeworks/ui/fragments/auth/AuthFragment.kt
@@ -1,0 +1,240 @@
+package com.chat.homeworks.ui.fragments.auth
+
+import android.content.Context
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.chat.homeworks.R
+import com.chat.homeworks.databinding.FragmentAuthBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.task.homework_4.data.local.preferences.PreferencesManager
+import com.vicmikhailau.maskededittext.MaskedFormatter
+import com.vicmikhailau.maskededittext.MaskedWatcher
+import java.util.concurrent.TimeUnit
+
+class AuthFragment : Fragment(R.layout.fragment_auth) {
+    private val binding by viewBinding(FragmentAuthBinding::bind)
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    private val formatter = MaskedFormatter("### ### ###")
+
+    private var phone = ""
+
+    private val preferencesManager by lazy {
+        PreferencesManager(
+            requireContext().getSharedPreferences(
+                "chat.preferences",
+                Context.MODE_PRIVATE
+            )
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        setMaskToEditText()
+        enableButtonDependingOnThePhoneNumberLength()
+        sendVerificationCodeAndProceed()
+        setupChanged()
+    }
+
+    private fun setMaskToEditText() {
+        binding.includeAuth.etPhoneNumber.addTextChangedListener(
+            MaskedWatcher(
+                formatter,
+                binding.includeAuth.etPhoneNumber
+            )
+        )
+    }
+
+    private fun enableButtonDependingOnThePhoneNumberLength() {
+        binding.includeAuth.etPhoneNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.includeAuth.btnSendVerificationCode.isEnabled =
+                    when (s.toString().length == 11) {
+                        true -> {
+                            binding.includeAuth.btnSendVerificationCode.setBackgroundColor(
+                                ContextCompat.getColor(
+                                    requireContext(),
+                                    R.color.purple_500
+                                )
+                            )
+                            true
+                        }
+                        false -> {
+                            binding.includeAuth.btnSendVerificationCode.setBackgroundColor(
+                                ContextCompat.getColor(
+                                    requireContext(),
+                                    R.color.gray
+                                )
+                            )
+                            false
+                        }
+                    }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun setupChanged() {
+        binding.includeAccept.pvVerificationCode.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length == 6) {
+                    hideSoftKeyboard()
+                    signInWithPhoneAuthCredential(verifyPhoneNumberWithCode(s.toString()))
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    fun verifyPhoneNumberWithCode(
+        code: String,
+    ) =
+        PhoneAuthProvider.getCredential(preferencesManager.verificationId.toString(), code)
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) =
+        with(binding.includeAccept.pvVerificationCode) {
+            firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        preferencesManager.isAuthenticated = true
+                        setHideLineWhenFilled(false)
+                        setLineColor(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                R.color.green
+                            )
+                        )
+                        handler.postDelayed({
+                            findNavController().navigate(R.id.chatsFragment)
+                        }, 1000L)
+                    } else {
+                        if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                            text?.clear()
+                            setLineColor(
+                                ContextCompat.getColor(
+                                    requireContext(),
+                                    R.color.red
+                                )
+                            )
+                            handler.postDelayed({
+                                setLineColor(
+                                    ContextCompat.getColor(
+                                        requireContext(), R.color.gray
+                                    )
+                                )
+                            }, 2000L)
+                        }
+                    }
+                }
+        }
+
+
+    private fun sendVerificationCodeAndProceed() {
+        binding.includeAuth.btnSendVerificationCode.setOnClickListener {
+            hideSoftKeyboard()
+            phone = binding.includeAuth.tlPhoneNumber.prefixText.toString() + formatter.formatString(
+                    binding.includeAuth.etPhoneNumber.text.toString()
+                )?.unMaskedString.toString()
+            val options =
+                PhoneAuthOptions.newBuilder(firebaseAuth)
+                    .setPhoneNumber(phone)
+                    .setTimeout(60L, TimeUnit.SECONDS)
+                    .setActivity(requireActivity())
+                    .setCallbacks(providePhoneAuthCallbacks())
+                    .build()
+            PhoneAuthProvider.verifyPhoneNumber(options)
+        }
+    }
+
+    private fun providePhoneAuthCallbacks() =
+        object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {}
+
+            override fun onVerificationFailed(e: FirebaseException) {
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    Toast.makeText(
+                        requireContext(),
+                        e.localizedMessage?.toString() ?: "An error occurred!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (e is FirebaseTooManyRequestsException) {
+                    Toast.makeText(
+                        requireContext(),
+                        e.localizedMessage?.toString() ?: "An error occurred!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken,
+            ) {
+                Toast.makeText(
+                    requireContext(),
+                    "Verification code has been sent to your phone number",
+                    Toast.LENGTH_LONG
+                ).show()
+                binding.acceptContainer.isVisible = true
+                binding.authContainer.isVisible = false
+                preferencesManager.verificationId = verificationId
+                saveUserData()
+            }
+        }
+
+    private fun saveUserData() {
+        val uid = firebaseAuth.currentUser?.uid
+
+
+        if (uid != null) {
+            val ref = FirebaseFirestore.getInstance().collection("Users").document(uid)
+            val userData = hashMapOf<String, String>()
+            userData["phone"] = binding.includeAuth.tlPhoneNumber.prefixText.toString() + formatter.formatString(
+                binding.includeAuth.etPhoneNumber.text.toString()
+            )?.unMaskedString.toString()
+
+
+            ref.set(userData).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    findNavController().navigateUp()
+                }
+            }
+        }
+    }
+
+    private fun Fragment.hideSoftKeyboard() {
+        val inputMethodManager =
+            context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+
+    }
+}
+
+

app/src/main/kotlin/com/chat/homeworks/ui/fragments/chats/ChatsFragment.kt
@@ -1,0 +1,44 @@
+package com.chat.homeworks.ui.fragments.chats
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.chat.homeworks.R
+import com.chat.homeworks.data.models.Users
+import com.chat.homeworks.databinding.FragmentChatsBinding
+import com.chat.homeworks.ui.fragments.chats.adapter.ChatsAdapter
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
+import com.task.homework_4.data.local.preferences.PreferencesManager
+
+class ChatsFragment : Fragment(R.layout.fragment_chats) {
+    private val binding by viewBinding(FragmentChatsBinding::bind)
+
+    private val data = arrayListOf<Users>()
+    private lateinit var adapter: ChatsAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val ref = FirebaseFirestore.getInstance().collection("Users")
+        adapter = ChatsAdapter()
+        binding.rvChat.adapter = adapter
+
+        ref.get().addOnCompleteListener {
+            if (it.isSuccessful) {
+                for (item in it.result.documents) {
+                    val user = item.toObject(Users::class.java)
+                   if (user!=null) {
+                       data.add(user)
+                   }
+                }
+
+                adapter.addUsers(data)
+
+            }
+        }
+    }
+}

app/src/main/kotlin/com/chat/homeworks/ui/fragments/chats/adapter/ChatsAdapter.kt
@@ -1,0 +1,38 @@
+package com.chat.homeworks.ui.fragments.chats.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.chat.homeworks.data.models.Users
+import com.chat.homeworks.databinding.ItemUserBinding
+import com.google.firebase.firestore.auth.User
+
+class ChatsAdapter: RecyclerView.Adapter<ChatsAdapter.ChatsViewHolder>() {
+
+    private val list: ArrayList<Users> = arrayListOf()
+
+    fun addUsers(newData:ArrayList<Users>) {
+        list.clear()
+        list.addAll(newData)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ChatsViewHolder(
+        ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+
+    override fun onBindViewHolder(holder: ChatsViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    inner class ChatsViewHolder(private val binding: ItemUserBinding) : ViewHolder(binding.root) {
+        fun bind(userInfo: Users) {
+            binding.tvPhone.text = userInfo.phone
+        }
+    }
+}
+

adzhibaevtan/homework-4.7

app/src/main/kotlin/com/reg/homeworks/data/models/Users.kt
@@ -1,0 +1,6 @@
+package com.reg.homeworks.data.models
+
+data class Users(
+    var phone: String? = null,
+    var uid: String? = null
+)

app/src/main/kotlin/com/reg/homeworks/ui/fragments/auth/AuthFragment.kt
@@ -1,0 +1,239 @@
+package com.reg.homeworks.ui.fragments.auth
+
+import android.content.Context
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.reg.homeworks.R
+import com.reg.homeworks.databinding.FragmentAuthBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.task.homework_4.data.local.preferences.PreferencesManager
+import com.vicmikhailau.maskededittext.MaskedFormatter
+import com.vicmikhailau.maskededittext.MaskedWatcher
+import java.util.concurrent.TimeUnit
+
+class AuthFragment : Fragment(R.layout.fragment_auth) {
+    private val binding by viewBinding(FragmentAuthBinding::bind)
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    private val formatter = MaskedFormatter("### ### ###")
+
+    private var phone = ""
+
+    private val preferencesManager by lazy {
+        PreferencesManager(
+            requireContext().getSharedPreferences(
+                "chat.preferences",
+                Context.MODE_PRIVATE
+            )
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        setMaskToEditText()
+        enableButtonDependingOnThePhoneNumberLength()
+        sendVerificationCodeAndProceed()
+        setupChanged()
+    }
+
+    private fun setMaskToEditText() {
+        binding.includeAuth.etPhoneNumber.addTextChangedListener(
+            MaskedWatcher(
+                formatter,
+                binding.includeAuth.etPhoneNumber
+            )
+        )
+    }
+
+    private fun enableButtonDependingOnThePhoneNumberLength() {
+        binding.includeAuth.etPhoneNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.includeAuth.btnSendVerificationCode.isEnabled =
+                    when (s.toString().length == 11) {
+                        true -> {
+                            binding.includeAuth.btnSendVerificationCode.setBackgroundColor(
+                                ContextCompat.getColor(
+                                    requireContext(),
+                                    R.color.purple_500
+                                )
+                            )
+                            true
+                        }
+                        false -> {
+                            binding.includeAuth.btnSendVerificationCode.setBackgroundColor(
+                                ContextCompat.getColor(
+                                    requireContext(),
+                                    R.color.gray
+                                )
+                            )
+                            false
+                        }
+                    }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun setupChanged() {
+        binding.includeAccept.pvVerificationCode.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length == 6) {
+                    hideSoftKeyboard()
+                    signInWithPhoneAuthCredential(verifyPhoneNumberWithCode(s.toString()))
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    fun verifyPhoneNumberWithCode(
+        code: String,
+    ) =
+        PhoneAuthProvider.getCredential(preferencesManager.verificationId.toString(), code)
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) =
+        with(binding.includeAccept.pvVerificationCode) {
+            firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        preferencesManager.isAuthenticated = true
+                        setHideLineWhenFilled(false)
+                        setLineColor(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                R.color.green
+                            )
+                        )
+                        handler.postDelayed({
+                            findNavController().navigate(R.id.chatsFragment)
+                        }, 1000L)
+                    } else {
+                        if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                            text?.clear()
+                            setLineColor(
+                                ContextCompat.getColor(
+                                    requireContext(),
+                                    R.color.red
+                                )
+                            )
+                            handler.postDelayed({
+                                setLineColor(
+                                    ContextCompat.getColor(
+                                        requireContext(), R.color.gray
+                                    )
+                                )
+                            }, 2000L)
+                        }
+                    }
+                }
+        }
+
+
+    private fun sendVerificationCodeAndProceed() {
+        binding.includeAuth.btnSendVerificationCode.setOnClickListener {
+            hideSoftKeyboard()
+            phone = binding.includeAuth.tlPhoneNumber.prefixText.toString() + formatter.formatString(
+                    binding.includeAuth.etPhoneNumber.text.toString()
+                )?.unMaskedString.toString()
+            val options =
+                PhoneAuthOptions.newBuilder(firebaseAuth)
+                    .setPhoneNumber(phone)
+                    .setTimeout(60L, TimeUnit.SECONDS)
+                    .setActivity(requireActivity())
+                    .setCallbacks(providePhoneAuthCallbacks())
+                    .build()
+            PhoneAuthProvider.verifyPhoneNumber(options)
+        }
+    }
+
+    private fun providePhoneAuthCallbacks() =
+        object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {}
+
+            override fun onVerificationFailed(e: FirebaseException) {
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    Toast.makeText(
+                        requireContext(),
+                        e.localizedMessage?.toString() ?: "An error occurred!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (e is FirebaseTooManyRequestsException) {
+                    Toast.makeText(
+                        requireContext(),
+                        e.localizedMessage?.toString() ?: "An error occurred!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken,
+            ) {
+                Toast.makeText(
+                    requireContext(),
+                    "Verification code has been sent to your phone number",
+                    Toast.LENGTH_LONG
+                ).show()
+                binding.acceptContainer.isVisible = true
+                binding.authContainer.isVisible = false
+                preferencesManager.verificationId = verificationId
+                saveUserData()
+            }
+        }
+
+    private fun saveUserData() {
+        val uid = firebaseAuth.currentUser?.uid
+
+
+        if (uid != null) {
+            val ref = FirebaseFirestore.getInstance().collection("Users").document(uid)
+            val userData = hashMapOf<String, String>()
+            userData["phone"] = binding.includeAuth.tlPhoneNumber.prefixText.toString() + formatter.formatString(
+                binding.includeAuth.etPhoneNumber.text.toString()
+            )?.unMaskedString.toString()
+
+            ref.set(userData).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    findNavController().navigateUp()
+                }
+            }
+        }
+    }
+
+    private fun Fragment.hideSoftKeyboard() {
+        val inputMethodManager =
+            context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+
+    }
+}
+
+

app/src/main/kotlin/com/reg/homeworks/ui/fragments/chats/ChatsFragment.kt
@@ -1,0 +1,41 @@
+package com.reg.homeworks.ui.fragments.chats
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.reg.homeworks.R
+import com.reg.homeworks.data.models.Users
+import com.reg.homeworks.databinding.FragmentChatsBinding
+import com.reg.homeworks.ui.fragments.chats.adapter.ChatsAdapter
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ChatsFragment : Fragment(R.layout.fragment_chats) {
+    private val binding by viewBinding(FragmentChatsBinding::bind)
+
+    private val data = arrayListOf<Users>()
+    private lateinit var adapter: ChatsAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val ref = FirebaseFirestore.getInstance().collection("Users")
+        adapter = ChatsAdapter()
+        binding.rvChat.adapter = adapter
+
+        ref.get().addOnCompleteListener {
+            if (it.isSuccessful) {
+                for (item in it.result.documents) {
+                    val user = item.toObject(Users::class.java)
+                   if (user!=null) {
+                       data.add(user)
+                   }
+                }
+
+                adapter.addUsers(data)
+
+            }
+        }
+    }
+}

app/src/main/kotlin/com/reg/homeworks/ui/fragments/chats/adapter/ChatsAdapter.kt
@@ -1,0 +1,37 @@
+package com.reg.homeworks.ui.fragments.chats.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.reg.homeworks.data.models.Users
+import com.reg.homeworks.databinding.ItemUserBinding
+
+class ChatsAdapter: RecyclerView.Adapter<ChatsAdapter.ChatsViewHolder>() {
+
+    private val list: ArrayList<Users> = arrayListOf()
+
+    fun addUsers(newData:ArrayList<Users>) {
+        list.clear()
+        list.addAll(newData)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ChatsViewHolder(
+        ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+
+    override fun onBindViewHolder(holder: ChatsViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    inner class ChatsViewHolder(private val binding: ItemUserBinding) : ViewHolder(binding.root) {
+        fun bind(userInfo: Users) {
+            binding.tvPhone.text = userInfo.phone
+        }
+    }
+}
+